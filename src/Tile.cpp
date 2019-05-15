f837a993d6ef707721b0a672b8914a9825f68137
Justin Seymour <jtseymour99@gmail.com>
2019-05-15 15:16:36 +1000

    Change tile class

--- src/Tile.cpp
+++ src/Tile.cpp
@@ -4,31 +4,7 @@
 Tile::Tile(){
 }
 
-Tile::Tile(char colour, int shape){
+Tile::Tile(Colour colour, Shape shape){
     this->colour= colour;
     this->shape= shape;
-}
-
-String Tile::getValue() {
-    return String(colour, 1) + std::to_string(shape);
-}
-
-char Tile::getColour() {
-    return colour;
-}
-
-int Tile::getShape() {
-    return getShape();
-}
-
-void Tile::setColour(char colour) {
-    this->colour= colour;
-}
-
-void Tile::setShape(int shape) {
-    this->shape= shape;
-}
-
-bool Tile::isPlacementValid() {
-    return true;
 }
