12fb923dd4f4bae5dc9a721ecaf81f1cbfa64020
Luke Di Guglielmo <s3724928@student.rmit.edu.au>
2019-05-18 05:06:37 +1000

    more placeTile

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -10,8 +10,9 @@ using std::cin;
 using std::stoi;
 
 GameEngine::GameEngine() {
-    turn= 0;
-    inGame= true;
+    // counter for turns passed in game
+    turn = 1;
+    inGame = true;
     startGame();
 }
 
@@ -81,7 +82,104 @@ void GameEngine::takeTurn() {
 }
 
 bool GameEngine::placeTile(Tile tile, std::string coordinate) {
-  return false;
+
+    // row player asked for
+    char row = coordinate.at(0);
+    int destinationRow = row - 65;
+    // column player asked for
+    std::string column = coordinate.substring(1);
+    int destinationColumn = std::stoi(column);
+    // copies of these to use as counters
+    int currentRow = destinationRow;
+    int currentColumn = destinationColumn;
+    Tile currentTile = tile;
+    // bool values used for checksm and rule variable -> -1 is colour, 1 is shape
+    bool emptyNorth = false, emptySouth = false, emptyEast = false, emptyWest = false;
+    int ruleNorth = 0, ruleSouth = 0, ruleEast = 0, ruleWest = 0;
+    // required colour and/or shape
+    std::string reqColour = tile->getValue().at(0); // eg. "R"
+    std::string reqShape = tile->getValue().at(1); // eg. "3"
+
+    bool isValid = true;
+    // check coordinate exists - needs to be changed for dynamic boards in future
+    if(destinationRow < 65 || destinationRow > 65 + BOARD_LENGTH || destinationColumn < 0 || destinationColumn > BOARD_LENGTH) isValid = false;
+    // check coordinate is not currently occupied
+    if(board[destinationRow][destinationColumn]!= NULL) isValid = false;
+    // check tile is placed adjacent to an existing tile after turn 1
+    // assuming turn denotes turns passed
+    emptyNorth = (board[destinationRow-1][destinationColumn] == NULL || destinationRow == 0) ? true : false;
+    emptySouth = (board[destinationRow+1][destinationColumn] == NULL || destinationRow == (BOARD_LENGTH - 1)) ? true : false;
+    emptyEast = (board[destinationRow][destinationColumn+1] == NULL || destinationColumn == (BOARD_LENGTH - 1)) ? true : false;
+    emptyWest = (board[destinationRow][destinationColumn-1] == NULL || destinationColumn == 0) ? true : false;
+    if(turn > 1 && emptyN && emptyS && emptyE && emptyW) isValid = false;
+
+    // check north
+    if(!emptyNorth) {
+        currentTile = board[currentRow-1][currentColumn];
+        if(currentTile->getValue().at(0).compare(reqColour)) ruleNorth--;
+        if(currentTile->getValue().at(1).compare(reqShape))  ruleNorth++;
+        if(ruleNorth == 0) isValid = false;
+    }
+    while(currentTile != NULL && (currentRow-1) >= 0) {
+        currentTile = board[currentRow-1][currentColumn];
+        if(tile->getValue().compare(currentTile->getValue()) == 0) isValid = false;
+        currentRow--;
+    }
+    // check south
+    if(!emptySouth) {
+        currentTile = board[currentRow+1][currentColumn];
+        if(currentTile->getValue().at(0).compare(reqColour)) ruleSouth--;
+        if(currentTile->getValue().at(1).compare(reqShape))  ruleSouth++;
+        if(ruleSouth == 0) isValid = false;
+    }
+    while(currentTile != NULL && (currentRow+1) <= BOARD_LENGTH) {
+        currentTile = board[currentRow+1][currentColumn];
+        if(tile->getValue().compare(currentTile->getValue()) == 0) isValid = false;
+        currentRow++;
+    }
+    // if both exist, determine if the same rule
+    if(!emptyNorth && !emptySouth) {
+        if(ruleNorth != ruleSouth) isValid = false;
+        // need method to determine if any tiles in south exist in north,if so isValid = false
+
+        // implement
+    }
+
+    // check east
+    if(!emptyEast) {
+        currentTile = board[currentRow][currentColumn+1];
+        if(currentTile->getValue().at(0).compare(reqColour)) ruleEast--;
+        if(currentTile->getValue().at(1).compare(reqShape))  ruleEast++;
+        if(ruleEast == 0) isValid = false;
+    }
+    while(currentTile != NULL && (currentColumn+1) <= BOARD_LENGTH) {
+        currentTile = board[currentRow][currentColumn+1];
+        if(tile->getValue().compare(currentTile->getValue()) == 0) isValid = false;
+        currentColumn++;
+    }
+    // check west
+    if(!emptyWest) {
+        currentTile = board[currentRow][currentColumn-1];
+        if(currentTile->getValue().at(0).compare(reqColour)) ruleWest--;
+        if(currentTile->getValue().at(1).compare(reqShape))  ruleWest++;
+        if(ruleWest == 0) isValid = false;
+    }
+    while(currentTile != NULL && (currentColumn+1) <= BOARD_LENGTH) {
+        currentTile = board[currentRow][currentColumn+1];
+        if(tile->getValue().compare(currentTile->getValue()) == 0) isValid = false;
+        currentColumn++;
+    }
+    // if both exist, determine if the same rule
+    if(!emptyEast && !emptyWest) {
+        if(ruleEast != ruleWest) isValid = false;
+        // need method to determine if any tiles in east exist in west,if so isValid = false
+
+        //implement
+    }
+
+
+    if(isValid) board[destinationRow][destinationColumn] = tile;
+    return isValid;
 }
 
 void GameEngine::saveGame(){
@@ -123,7 +221,7 @@ void GameEngine::drawTile() {
 }
 
 void GameEngine::endTurn() {
-    turn= abs(turn-1);
+    turn++;
 }
 
 //prints the entire board to the system console including the tiles placed
